cd44c480523b05a13966cc879cfbfdb939122870
Luiz Gustavo Marques <62360761+cmarquesluizgustavo@users.noreply.github.com>
2021-04-30 20:47:33 -0300

    Update Grafo.h

--- Grafo.h
+++ Grafo.h
@@ -1,6 +1,6 @@
 #include <bits/stdc++.h>
-#ifndef Grafo_hp
-#define Grafo_hp
+#ifndef Graph_hp
+#define Graph_hp
 using namespace std;
 
 struct Tree{
@@ -9,10 +9,10 @@ struct Tree{
   Tree* pai;
 };
     
-class Grafo{
+class Graph{
 public:
-    Grafo();
-    ~Grafo();
+    Graph();
+    ~Graph();
     
 protected:
     int vertices;
@@ -20,7 +20,7 @@ protected:
     int gmax;
     int gmin;
     int gmed;
-    int * vector_grau;
+    int * vector_degree;
 	  int maxLevel;
     int *visitedInThisExecution;
     int* *m_nodes_componentes_conexas;
@@ -34,32 +34,31 @@ protected:
     void mergeSort(int arr[], int a, int c);
     void Infos();
 };
-#endif /* Grafo_hpp */
+#endif /* graph_hpp */
 
 #ifndef List_h
 #define List_h
 
 struct ListNode{
     int vertice;
-    float peso;
     ListNode* pPrev;
     ListNode* pNext;
 };
 
-class Lista :
-	public Grafo{
+class List :
+	public Graph{
 		public:
-    		Lista(string path, string algorithm, int selectedVertice, int targetVertice);
-    		~Lista();
+    		List(string path, string algorithm, int selectedVertice, int targetVertice);
+    		~List();
 			protected:
     			stack<Tree*> pstack; //pilha p DFS
     			queue<Tree*> fqueue;   //fila p BFS
     			int *visited;   //vetor de marcação
     			void addAresta(int de, int para);   
-    			ListNode** m_pLista;
-    			void Grau();
-    			void BFS_lista(int s, int targetVertice = 0);
-    			void DFS_lista(int s);
+    			ListNode** m_pList;
+    			void Degree();
+    			void BFS_list(int s, int targetVertice = 0);
+    			void DFS_list(int s);
 				  void Diameter();
     			int m_tamanho_da_componente_conexa;
     			int m_numero_de_componentes_conexas = 0;
@@ -76,38 +75,37 @@ class Lista :
 #endif /* List_h */
 
 
-#ifndef Matriz_hp
-#define Matriz_hp
+#ifndef Matrix_hp
+#define Matrix_hp
 using namespace std::chrono;
 
-class Matriz :
-	public Grafo{
+class Matrix :
+	public Graph{
 		public:
-		    Matriz(string path, string algorithm, int selectedVertice, int targetVertice);
-		    ~Matriz();
+			Matrix(string path, string algorithm, int selectedVertice, int targetVertice);
+			~Matrix();
 		
 		private:
 		    void construtor();
 		    stack<Tree*> pstack;
-		    bool** matriz;
+		    bool** matrix;
 		    void addAresta(int i, int j);
-		    int * vector_grau;
+		    int * vector_degree;
 		    queue<Tree*> fqueue;
 		    int *visited;
-		    void BFS_Matriz(int s);
-		    void DFS_matriz(int s);
-				void Diameter();
+		    void BFS_Matrix(int s);
+		    void DFS_Matrix(int s);
+		    void Diameter();
 
 		    Tree * Parentesco(Tree * v, int w);
-    		int m_tamanho_da_componente_conexa;
-    		int m_numero_de_componentes_conexas = 0;
-    		int* m_componentes_conexos;
-    		int m_x;
-    		int m_maior_tamanho = 0;
-    		bool etapa;
-    		int m_i;
-        void FComponentes_conexas();
+    		    int m_tamanho_da_componente_conexa;
+    		    int m_numero_de_componentes_conexas = 0;
+    		    int* m_componentes_conexos;
+    		    int m_x;
+    		    int m_maior_tamanho = 0;
+    		    bool etapa;
+    		    int m_i;
+                    void FComponentes_conexas();
 		    std::chrono::time_point<std::chrono::system_clock> start, end;  
 	};
-#endif /* Matriz_hp */
-
+#endif /* Matrix_hp */
